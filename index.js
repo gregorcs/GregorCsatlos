5a243b4a44658d1b17e1c5d55c41a2fed1507827
Gregor Csatlós <g.csatlos1@gmail.com>
2022-04-09 21:39:28 +0200

    Added get request to github API
    for now prints to console

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // JavaScript source code
+
+const userAction = async () => {
+    const response = await fetch('https://api.github.com/users/gregorcs/repos');
+    const myJson = await response.json();
+    console.log("hello");
+}
+
