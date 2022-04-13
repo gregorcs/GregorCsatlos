170b575621e8016782a923a8be7cd3a931eda7fc
Gregor Csatlós <g.csatlos1@gmail.com>
2022-04-13 23:40:21 +0200

    Added repo list to main div

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 // JavaScript source code
 
-const userAction = async () => {
-    const response = await fetch('https://api.github.com/users/gregorcs/repos');
-    const myJson = await response.json();
-    console.log("hello");
+function getRepositories() {
+    const request = new XMLHttpRequest();
+    let arrayOfRepositories;
+    request.open("GET", "https://api.github.com/users/gregorcs/repos")
+    request.send();
+    request.onload = () => {
+        if (request.status == 200) {
+            console.log(JSON.parse(request.response));
+            arrayOfRepositories = JSON.parse(request.response);
+        }
+        makeRepositoriesInDivs(arrayOfRepositories);
+    }
+    return arrayOfRepositories;
 }
 
+function makeRepositoriesInDivs(arrayOfRepositories) {
+    var container = document.getElementById('projectsContainer');
+    var smallDiv;
+
+    for (let i = 0; i < arrayOfRepositories.length; i++) {
+        smallDiv = document.createElement("div");
+        var attr = document.createAttribute("id");
+        var name = document.createElement("p");
+        var description = document.createElement("p");
+        attr.value = "project";
+        smallDiv.setAttributeNode(attr);
+        name.innerHTML = arrayOfRepositories[i].name;
+        description.innerHTML = arrayOfRepositories[i].description;
+        smallDiv.appendChild(name);
+        smallDiv.appendChild(description);
+        container.appendChild(smallDiv);
+    }
+}
